f0da77599e7e49ca402c993603339c627d388028
Reinoldo Sommer <reinoldo@outlook.com>
2017-11-14 00:36:29 -0200

    agora sim é o vincular perfil

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,11 +12,12 @@ import { LoginComponent } from './login/login.component';
 import { PesquisarComponent } from './pesquisar/pesquisar.component';
 import { CadastraUsuarioComponent } from './cadastra-usuario/cadastra-usuario.component';
 import { VincularMuniciModule } from './vincular-munici/vincular-munici.module';
+import { VincularPerfilComponent } from './vincular-perfil/vincular-perfil.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-   VincularMuniciComponent, FormDebugComponent, LoginComponent, CadastraUsuarioComponent
+   VincularMuniciComponent, FormDebugComponent, LoginComponent, CadastraUsuarioComponent, VincularPerfilComponent
   ],
   imports: [
     BrowserModule,FormsModule, routing
