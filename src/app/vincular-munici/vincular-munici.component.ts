730a2c7be39d9aee9abcc654c89648b19af887d3
Reinoldo Oliveira Sommer <reinoldo.sommer@agrario.gov.br>
2017-11-27 16:10:18 -0200

    correção de bugs.

--- src/app/vincular-munici/vincular-munici.component.ts
+++ src/app/vincular-munici/vincular-munici.component.ts
@@ -26,20 +26,8 @@ export class VincularMuniciComponent implements OnInit {
   private cadastrosGS = []
   private cadastrosDAP = []
   private municipio: string[] = []
-<<<<<<< HEAD
  
   private estados: EstadoBr[];
-=======
-
-  private estados = [{ nome: 'Acre', uf: 'AC' }, { nome: 'Alagoas', uf: 'AL' }, { nome: 'Amapá', uf: 'AP' },
-  { nome: 'Amazonas', uf: 'AM' }, { nome: 'Bahia', uf: 'BA' }, { nome: 'Ceará', uf: 'CE' },
-  { nome: 'Distrito Federal', uf: 'DF' }, { nome: 'Espirito Santo', uf: 'ES' }, { nome: 'Goiás', uf: 'GO' }, { nome: 'Maranhão', uf: 'MA' },
-  { nome: 'Mato Grosso do Sul', uf: 'MS' }, { nome: 'Mato Grosso', uf: 'MT' }, { nome: 'Minas Gerais', uf: 'MG' }, { nome: 'Pará', uf: 'PA' },
-  { nome: 'Paraíba', uf: 'PB' }, { nome: 'Paraná', uf: 'PR' }, { nome: 'Pernambuco', uf: 'PE' }, { nome: 'Piauí', uf: 'PI' },
-  { nome: 'Rio de Janeiro', uf: 'RJ' }, { nome: 'Rio Grande do Norte', uf: 'RN' }, { nome: 'Rio Grande do Sul', uf: 'RS' }, { nome: 'Rondônia', uf: 'RO' },
-  { nome: 'Roraima', uf: 'RR' }, { nome: 'Santa Catarina', uf: 'SC' }, { nome: 'São Paulo', uf: 'SP' }, { nome: 'Sergipe', uf: 'SE' }, { nome: 'Tocantins', uf: 'TO' }
-  ];
->>>>>>> 70cf47d1da2a5fee0471f96988638dcb04520142
 
 
   constructor(private listaEstados: EstadosBrService, private formBuilder: FormBuilder) {
@@ -48,23 +36,6 @@ export class VincularMuniciComponent implements OnInit {
     
   
 
-<<<<<<< HEAD
-=======
-  public setCadastrar(dado){
-
-    this.cadastrar.push(dado);
-    
-  }
-  public retira(): void{
-    
-  }
-
-  public getCadastrar(){
-    return this.cadastrar;
-  }
-  public getAnos() {
-    return this.anos;
->>>>>>> 70cf47d1da2a5fee0471f96988638dcb04520142
   }
 
  
@@ -134,7 +105,6 @@ export class VincularMuniciComponent implements OnInit {
   }
   onSubmitGS() {
     // console.log(this.formularioGS);
-<<<<<<< HEAD
 
     // this.http.post(this.url, JSON.stringify(this.formularioGS.value))
     //   .map(res => res)
@@ -145,23 +115,10 @@ export class VincularMuniciComponent implements OnInit {
       // },
       // (error: any) => alert('erro'));
 
-=======
-   
-    // this.http.post(this.url, JSON.stringify(this.formularioGS.value))
-    //   .map(res => res)
-    //   .subscribe(dados => {
-    //     console.log(dados);
-    //     this.formularioGS.reset();
-    //   },
-    //   (error: any) => alert('erro'));
-    this.setCadastrar(this.formularioGS.value);
-    
->>>>>>> 70cf47d1da2a5fee0471f96988638dcb04520142
 
   }
   onSubmitDAP() {
     //console.log(this.formularioDAP);
-<<<<<<< HEAD
 
     // this.http.post(this.url, JSON.stringify(this.formularioDAP.value))
     
@@ -175,20 +132,6 @@ export class VincularMuniciComponent implements OnInit {
       // },
       // (error: any) => alert('erro'))
 
-=======
-    
-    // this.http.post(this.url, JSON.stringify(this.formularioDAP.value))
-    //   .map(res => res)
-    //   .subscribe(dados => {
-    //     console.log(dados);
-    //     this.formularioDAP.reset();
-    //   },
-    //   (error: any) => alert('erro'))
-
-    this.setCadastrar(this.formularioDAP.value);
-    console.log(this.cadastrar);
-       // this.formularioDAP.reset();
->>>>>>> 70cf47d1da2a5fee0471f96988638dcb04520142
   };
      
 
